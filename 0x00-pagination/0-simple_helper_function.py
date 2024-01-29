eebc8f0b845d13c710b424ade06bcff8dc7711d3
EmadAnwer <emadanwer.official@gmail.com>
2024-01-29 14:46:09 +0200

    [add] task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,6 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     return a tuple (start, end)
     """
-
+    if isinstance(page_size, int) != int or isinstance(page, int):
+        raise TypeError
     return ((page - 1) * page_size, page * page_size)
