b0bd83a81ac46228af9061955429673006a2db3d
EmadAnwer <emadanwer.official@gmail.com>
2024-01-29 14:52:26 +0200

    [add] task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,6 +8,4 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     return in a list for those particular pagination parameters
     """
-    if isinstance(page_size, int) != int or isinstance(page, int):
-        raise TypeError
     return ((page - 1) * page_size, page * page_size)
