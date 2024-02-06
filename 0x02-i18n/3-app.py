00e4780d253a199f4ca81009c7cce1f8959de64a
EmadAnwer <emadanwer.official@gmail.com>
2024-02-07 00:43:09 +0200

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,7 +26,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str | None:
+def get_locale() -> str:
     """Get locale from request"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
