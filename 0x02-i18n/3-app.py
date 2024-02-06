141d83fd932c20ee1db8e797bd356fc0953417d5
EmadAnwer <emadanwer.official@gmail.com>
2024-02-07 00:23:04 +0200

    [task 3]

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -12,7 +12,7 @@ class Config(object):
     """Config class"""
 
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
